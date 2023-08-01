70251b1fcf82a2617d420c015fe64cb36ba008f7
Wafie Abiyya <wafieabiyya7@gmail.com>
2023-08-01 14:34:48 +0700

    update: bulan dapat direset pada tahun selain 2023

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -72,6 +72,29 @@ export class CheckoutComponent implements OnInit {
     )
   }
 
+
+    handleMonthAndYears(){
+      const creditCardFormGroup = this.checkoutFormGroup.get('creditCard')
+
+      const currentYear: number = new Date().getFullYear()
+      const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear)
+
+      //if this year == selected year then start value month with current month
+      //else, start value month with first month
+      let startMonth: number;
+      if(currentYear == selectedYear){
+        startMonth = new Date().getMonth() + 1
+      } else{
+        startMonth = 1
+      }
+
+      this._dropDownService.getCreditCardMonths(startMonth).subscribe(
+        data =>{
+          console.log("Retrieved credit card month: "+ JSON.stringify(data))
+          this.creditCardMonths = data
+        }
+      )
+    }
   //copyToBillingAddress Method
   copyToBillingAddress(event: Event) {
     if (event.target instanceof HTMLInputElement) {
